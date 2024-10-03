2e17071c7af41f6b20d0eb8bd4cfa6a84a015bcd
Denis Ushakov <denisushakov56@gmail.com>
2024-10-03 16:53:23 +0300

    Deleted token

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../storage/scheduler.db" //"../scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZF9oYXNoIjoiNWU4ODQ4OThkYTI4MDQ3MTUxZDBlNTZmOGRjNjI5Mjc3MzYwM2QwZDZhYWJiZGQ2MmExMWVmNzIxZDE1NDJkOCJ9.TAdhLIYQhadNfB3MOpkGTD07A8tdXX7ue_ghYOif71w`
+var Token = ``
